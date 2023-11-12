15c0b41c289992de8ddec7ee118a303e04a9156a
DKurilich <kurilicd@oregonstate.edu>
2023-11-12 15:18:48 -0800

    initial commits

--- README.txt
+++ README.txt
@@ -5,7 +5,19 @@ This microservice will always be running in the background. When the tasks.csv i
 microservice will then begin. It will evaluate the tasks lists based on the criteria described in the
 task_list_microservice.py header comments and as given to me by my partner.
 Once the tasks have been prioritized in the microservice, they will be written in order from the highest priority to
-the lowest priority into the prioritized_tasks.csv file. Each task will be listed on a new line.
-The tasks.csv folder will be cleared once the prioritized_tasks.csv file is written.
+the lowest priority into the prioritized_tasks.csv file, with the priority score and task name and each line, separated
+by a comma. Each task will be listed on a new line.
+The tasks.csv file will not be modified in any way by this microservice.
 
+An example is shown as follows below. The microservice receives the following from the tasks.txt file
+
+Laundry,11-01-2023,9,7
+Dishes,11-01-2023,6,5
+Vacuuming,11-12-2023,10,10
+
+The prioritized_task list will then return the following (if today's date is 2023-11-12:
+
+10.0,Vacuuming
+7.3,Laundry
+5.7,Dishes
 
--- task_list_microservice.py
+++ task_list_microservice.py
@@ -5,9 +5,84 @@
 # Same day = 10
 # Next day = 9
 # 2 days = 8
-# 1 week - 4 weeks = 7-4 respectively
-# 4 weeks - 2 months = 3
-# 2 months - 6 months = 2
-# anything greater than 6 months = 1
+# 3 days - 7 days = 7
+# 8 days - 14 days = 6
+# 15 days - 21 days = 5
+# 22 days - 28 days = 4
+# 29 days -  60 days = 3
+# 61 days -  180 days = 2
+# 180 days < = 1
+# Priority score = [ due_date_score (1-10) + Urgency score (1-10) + Importance score (1-10) ] * .333
+# rounded to the nearest 10th
 
+import datetime
+import time
+from datetime import date
+from datetime import datetime, timedelta
 
+while True:
+
+    # open the tasks.txt file and copy the task list into a string
+    with open('tasks.txt', 'r', encoding="utf-8") as tasks:
+        task_list_string = tasks.read()
+    tasks.close()
+
+    # strip task character string of return line \n characters and convert each comma separated string into an item
+    # in a list variable
+    task_list = task_list_string.replace("\n", ",")
+    task_list = list(task_list.split(","))
+
+    # calculate today's date
+    # determine number of items in the task list to sort
+    today = date.today()
+    num_tasks = int(len(task_list) / 4)
+    priority_scores = []
+
+    # this for loop goes through each individual task and calculates its priority score
+    # the priority score and name of the task are added to the priority_scores list variable as a tuple (score,task)
+    for num in range(num_tasks):
+        item_num = 4 * num
+        priority_score = 0
+        priority_score += int(task_list[item_num + 2])
+        priority_score += int(task_list[item_num + 3])
+        date = datetime.strptime(task_list[item_num + 1], '%m-%d-%Y').date()
+        days_due = today - date
+        days_due = days_due.days
+        if days_due == 0:
+            priority_score += 10
+        elif days_due == 1:
+            priority_score += 9
+        elif days_due == 2:
+            priority_score += 8
+        elif 2 < days_due <= 7:
+            priority_score += 7
+        elif 8 <= days_due <= 14:
+            priority_score += 6
+        elif 15 <= days_due <= 21:
+            priority_score += 5
+        elif 22 <= days_due <= 28:
+            priority_score += 4
+        elif 29 <= days_due <= 60:
+            priority_score += 3
+        elif 61 <= days_due <= 180:
+            priority_score += 2
+        else:
+            priority_score += 1
+        priority_score = priority_score / 3
+        priority_score = round(priority_score, 1)
+        priority_scores.append((priority_score, task_list[item_num]))
+
+    # the priority score tuples in the priority_scores list are sorted in order from highest to lower
+    priority_scores.sort(reverse=True)
+
+    # priority output is created and a string a characters to print into the output .txt file is generated
+    priority_output = ""
+    for num in range(num_tasks):
+        task_to_add = priority_scores[num]
+        priority_output = priority_output + str(task_to_add[0]) + "," + str(task_to_add[1]) + "\n"
+
+    # this section writes in the prioritized tasks into the prioritized_tasks.txt file, one task and priority score
+    # per line
+    with open('prioritized_tasks.txt', 'w', encoding="utf-8") as prioritized_tasks:
+        prioritized_tasks.write(priority_output)
+    prioritized_tasks.close()
